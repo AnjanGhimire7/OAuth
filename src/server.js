22bcc8d4309cd9ee16b82d15665666b3a79d3404
AnjanGhimire7 <anjanghimire876@gmail.com>
2024-08-18 17:09:56 +0545

    adding pm2 for clustering and making api availabe offline

--- src/server.js
+++ src/server.js
@@ -8,27 +8,14 @@ import dotenv from "dotenv";
 dotenv.config({
   path: "./.env",
 });
-const totalCpu = os.cpus().length;
-if (cluster.isPrimary) {
-  console.log(`Primary ${process.pid} is runnning`);
-  //fork the workers
-  for (let i = 0; i < totalCpu; i++) {
-    cluster.fork();
-  }
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`Worker process ${worker.process.pid} died. Restarting...`);
-    cluster.fork();
-  });
-} else {
-  connectDB()
-    .then(() => {
-      const createServer = http.createServer(app);
-      createServer.listen(process.env.PORT || 5000, () => {
-        console.log(`server is running on the port:${process.env.PORT}`);
-      });
-    })
-    .catch((error) => {
-      console.log("Mongodb connection failed!!!", error);
+
+connectDB()
+  .then(() => {
+    const createServer = http.createServer(app);
+    createServer.listen(process.env.PORT || 5000, () => {
+      console.log(`server is running on the port:${process.env.PORT}`);
     });
-  console.log(`Worker ${process.pid} is running`);
-}
+  })
+  .catch((error) => {
+    console.log("Mongodb connection failed!!!", error);
+  });
